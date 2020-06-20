27c91ec18477a0db45db986ce8590d3c836f97a7
mir1198yusuf <34382235+mir1198yusuf@users.noreply.github.com>
2020-06-20 21:17:48 +0530

    Updated constants.js acc to production api name

--- frontend-reactjs/src/components/constants.js
+++ frontend-reactjs/src/components/constants.js
@@ -1,7 +1,14 @@
-//will be different in production
+//comment part was for development
 
+/*
 const API_URL = "http://127.0.0.1:8000/api/";
 const API_MEDIA_URL = "http://127.0.0.1:8000";
+*/
+
+//below part is for production
+const API_URL = "https://mir1198yusuf1.pythonanywhere.com/api/";
+const API_MEDIA_URL = "https://mir1198yusuf1.pythonanywhere.com";
 
 export default API_URL;
 export {API_MEDIA_URL};
+
